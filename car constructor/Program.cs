ea2f032eb1c9996e80a92f59a7ad52dfe51b2dfd
Дмитрий Чернецкий <0639651043w@gmail.com>
2020-04-28 21:25:49 +0300

    Добавьте файлы проекта.

---
+++ car constructor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car Car = new car("Audi", "A8", 2, 2000);
+            Car.GetInfo();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ car constructor/car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_constructor
+{
+    class car
+    {
+        public string name;
+        public string model;
+        public int age;
+        public int weight;
+
+        public car(string n, string m, int a, int w) { name = n; model = m; age = a; weight = w; }
+        public void GetInfo()
+        {
+            Console.WriteLine($"marka: {name} model: {model} vozrast: {age} ves: {weight}");
+        }
+    }
+}
