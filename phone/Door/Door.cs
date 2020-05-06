6b8a7fd47e6d624f21d0d46e9dad46ccc9d51f8c
Дмитрий Чернецкий <0639651043w@gmail.com>
2020-05-06 19:54:31 +0300

    1

---
+++ phone/Door/Door.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Door
+{
+    class Door
+    {
+        public Ruchka ruchka { get; set; }
+        public int ves { get; set; }
+        public Door(Ruchka ruchka1, int ves1)
+        {
+            ruchka = ruchka1;
+            ves = ves1;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"vess: {ves}, ruchkaColor: {ruchka.color} ");
+           
+        }
+        
+    }
+}
---
+++ phone/Door/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Door
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ruchka ruchka = new Ruchka();
+            ruchka.color = "black";
+
+            Door door = new Door(ruchka, 30);
+            door.GetInfo();
+            Console.ReadLine();
+
+        }
+    }
+}
